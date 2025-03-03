c7fdb67807551b1862dcffd378b115a32d2900c6
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-03-03 23:32:20 +0500

    Configurations

--- ExpenseTracker/Configuration/TransactionConfiguration.cs
+++ ExpenseTracker/Configuration/TransactionConfiguration.cs
@@ -1,8 +1,44 @@
 ﻿using System;
+using System.Transactions;
+using ExpenseTracker.Enums;
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class TransactionConfiguration
+public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
 {
-	public TransactionConfiguration()
-	{
-	}
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.HasKey(t => t.Id);
+
+        builder.Property(t => t.UserId)
+               .IsRequired();
+
+        builder.Property(t => t.CategoryId)
+               .IsRequired();
+
+        builder.Property(t => t.Amount)
+               .IsRequired()
+               .HasColumnType("decimal(18,2)");
+
+        builder.Property(t => t.Type)
+               .HasDefaultValue(TransactionType.Expense);
+
+        builder.Property(t => t.Date)
+               .HasDefaultValueSql("GETDATE()");
+
+        builder.Property(t => t.Note)
+               .HasMaxLength(500);
+
+        builder.HasOne(t => t.User)
+               .WithMany(u => u.Transactions)
+               .HasForeignKey(t => t.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(t => t.Category)
+               .WithMany(c => c.Transaction)
+               .HasForeignKey(t => t.CategoryId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+
 }
