b766f3500e40996333f658ed516c5157ff2d460a
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-03-03 23:33:30 +0500

    Context

--- ExpenseTracker/Context/ExpenseDbContext.cs
+++ ExpenseTracker/Context/ExpenseDbContext.cs
@@ -1,11 +1,26 @@
 ﻿using ExpenseTracker.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using System.Collections.Generic;
 
 namespace ExpenseTracker.Context
 {
     public class ExpenseDbContext : DbContext
     {
-        
+        public ExpenseDbContext(DbContextOptions<ExpenseDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+        }
     }
 }
