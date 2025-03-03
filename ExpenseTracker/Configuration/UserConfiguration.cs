bed6fda7956bd9ee42466793b708796796f8289d
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-03-03 23:32:41 +0500

    Configurations

--- ExpenseTracker/Configuration/UserConfiguration.cs
+++ ExpenseTracker/Configuration/UserConfiguration.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using ExpenseTracker.Models;
+using ExpenseTracker.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class UserConfiguration : IEntityTypeConfiguration<>
+public class UserConfiguration : IEntityTypeConfiguration<User>
 {
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
 
-	public UserConfiguration()
-	{
-	}
+        builder.Property(u => u.Email)
+               .IsRequired()
+               .HasMaxLength(255);
 
+        builder.Property(u => u.PasswordHash)
+               .IsRequired()
+               .HasMaxLength(255);
+
+        builder.Property(u => u.CreatedAt)
+               .HasDefaultValueSql("GETDATE()");
+
+        builder.HasMany(u => u.Categories)
+               .WithOne(c => c.User)
+               .HasForeignKey(c => c.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(u => u.Transactions)
+               .WithOne(t => t.User)
+               .HasForeignKey(t => t.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
 }
