4c885a6dbfe7d7792f547ce2be3f4ceca0b8c743
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-03-03 23:32:01 +0500

    Configurations

--- ExpenseTracker/Configuration/CategoryConfiguration.cs
+++ ExpenseTracker/Configuration/CategoryConfiguration.cs
@@ -1,8 +1,30 @@
 ﻿using System;
+using ExpenseTracker.Enums;
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class CategoryConfiguration
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
 {
-	public CategoryConfiguration()
+	public void Configure(EntityTypeBuilder<Category> builder)
 	{
-	}
+		builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.UserId)
+                   .IsRequired();
+
+        builder.Property(c => c.Type)
+               .HasDefaultValue(TransactionType.Expense);
+
+        builder.HasOne(c => c.User)
+               .WithMany(u => u.Categories)
+               .HasForeignKey(c => c.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(c => c.Transaction)
+               .WithOne(t => t.Category)
+               .HasForeignKey(t => t.CategoryId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+	
 }
