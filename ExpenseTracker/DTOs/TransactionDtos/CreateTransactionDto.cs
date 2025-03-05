0cb07efa5a192a6bc3389f2def8a6de3bd3da388
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-03-05 23:59:02 +0500

    DTOs

---
+++ ExpenseTracker/DTOs/TransactionDtos/CreateTransactionDto.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseTracker.Enums;
+
+namespace ExpenseTracker.DTOs.TransactionDtos
+{
+    public class CreateTransactionDto
+    {
+        public Guid CategoryId { get; set; }
+        public decimal Amount { get; set; }
+        public TransactionType Type { get; set; }
+        public DateTime Date {  get; set; }
+        public string Note { get; set; }
+    }
+}
