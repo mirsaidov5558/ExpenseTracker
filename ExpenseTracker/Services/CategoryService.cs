18c59f949b3c90c7998e76141342ba6462d7d41b
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-03-06 00:04:44 +0500

    Services

---
+++ ExpenseTracker/Services/CategoryService.cs
@@ -0,0 +1,64 @@
+﻿using ExpenseTracker.DTOs.CategoryDtos;
+using ExpenseTracker.Exception;
+using ExpenseTracker.Interfaces.Repositories;
+using ExpenseTracker.Interfaces.Services;
+using ExpenseTracker.Models;
+using System;
+
+public class CategoryService : ICategoryService
+{
+    public readonly ICategoryRepository _categoryRepository;
+    public CategoryService(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+    public async Task AddAsync(CreateCategoryDto categoryDto)
+    {
+        var category = new Category
+        {
+            Name = categoryDto.Name,
+        };
+        await _categoryRepository.AddAsync(category);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await _categoryRepository.DeleteAsync(id);
+    }
+
+    public async Task<IEnumerable<CategoryDto>> GetAllAsync()
+    {
+        var categories = await _categoryRepository.GetAllAsync();
+        var categoryDtos = new List<CategoryDto>();
+        foreach (var categoryDto in categories)
+        {
+            categoryDtos.Add(new CategoryDto { Id = categoryDto.Id, Name = categoryDto.Name });
+
+        }
+        return categoryDtos;
+    }
+
+    public async Task<CategoryDto> GetByIdAsync(Guid Id)
+    {
+        var category = await _categoryRepository.GetByIdAsync(Id);
+        if (category != null)
+        {
+            return new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+        }
+        return null;
+    }
+
+    public async Task UpdateAsync(UpdateCategoryDto categoryDto)
+    {
+        var category = new Category
+        {
+            Id = categoryDto.Id,
+            Name = categoryDto.Name
+        };
+        await _categoryRepository.UpdateAsync(category);
+    }
+}
---
+++ ExpenseTracker/Services/JwtService.cs
@@ -0,0 +1,41 @@
+﻿using ExpenseTracker.Interfaces.Services;
+using ExpenseTracker.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ExpenseTracker.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Name, user.Email)
+        };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Secret"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(int.Parse(_configuration["JwtSettings:ExpirationMinutes"])),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
